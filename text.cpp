36cc1d21434415db71a6da3ae819ef89ed90de38
JamesGreyThompson <JamesGreyThompson@users.noreply.github.com>
2018-02-14 11:47:13 -0400

    Up to text abc

--- text.cpp
+++ text.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main( )
@@ -8,6 +9,20 @@ int main( )
    float b = 68.123; 
    char c = 'J';
  
+ofstream out{"abc.txt"};
+if (out)
+{
+out <<a<<" "<<b<<" "<<c;
+}else 
+cout <<"Error writing file";
+//out.write (reinterpet_cast <char *> (&a), sizeof (a));
+//out.write (reinterpet_cast <char *> (&b), sizeof (b));
+//out.write (reinterpet_cast <char *> (&c), sizeof (c));
+
+
+
+
+
    cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
 
    cout<<endl<<endl;
